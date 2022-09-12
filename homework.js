202c3e7c6c56035e645d8e571fc2786a69071ed1
kal240392 <101008196+kal240392@users.noreply.github.com>
2022-09-12 20:59:39 +0200

    first commit

---
+++ homework.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  let resultado = 0;
+
+  for(let i = 0; i < num.length; i++){
+    resultado += +num[i] * 2 ** (num.length - 1 - i);
+  }
+  return resultado;
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
